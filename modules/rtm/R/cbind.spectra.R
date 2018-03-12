8caeed5882baf226fc2d90e3ea6461ed834432b3
Alexey Shiklomanov <ashiklom@bu.edu>
2018-03-12 16:14:59 -0400

    RTM: Preserve spectra_type in `cbind.spectra`

--- modules/rtm/R/cbind.spectra.R
+++ modules/rtm/R/cbind.spectra.R
@@ -7,8 +7,15 @@ cbind.spectra <- function(...) {
   lens <- vapply(dots, ncol, numeric(1))
   waves <- lapply(dots, wavelengths)
   new_waves <- Reduce(union, waves)
+  types <- lapply(dots, spectra_type)
+  types2 <- Map(rep_if_one, types, lens)
+  new_types <- Reduce(c, types2)
   names_list <- lapply(dots, colnames)
-  m <- spectra(matrix(NA_real_, length(new_waves), sum(lens)), new_waves)
+  m <- spectra(
+    matrix(NA_real_, length(new_waves), sum(lens)),
+    wavelengths = new_waves,
+    type = new_types
+  )
   colnames(m) <- as.character(seq(1, sum(lens)))
   j <- 1
   for (i in seq_along(dots)) {
@@ -20,4 +27,18 @@ cbind.spectra <- function(...) {
     j <- j + lens[i]
   }
   m
+}
+
+rep_if_one <- function(x, n) {
+  if (length(x) == 1) {
+    rep(x, n)
+  } else if (length(x) == n) {
+    x
+  } else {
+    stop(
+      "x must be length 1 or n. ",
+      "Current x has length ", length(x),
+      " but n value is ", n, "."
+    )
+  }
 }
